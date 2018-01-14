dfb3149a09c1462b264dd40ac31f1dc9701200a5
tan-t <lee30x@gmail.com>
2018-01-14 23:42:31 +0900

    readme修正

--- renderer/language/commands/comment.js
+++ renderer/language/commands/comment.js
@@ -6,3 +6,8 @@ OneLineEditor.Comment = function(option){
   
   OneLineEditor.Interpreter.register('#',OneLineEditor.Comment);
   
+  OneLineEditor.Help.register('#',{
+    'commandHelp' : 'コメントをパイプラインに記入します。内部的には、入力をそのまま出力にパイプします。\n主に、お気に入り内を管理するためにタグ的に入れる用法を想定しています。',
+    'argHelps':[
+    ]
+  })
